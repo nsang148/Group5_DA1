e3d4e44b57ff29e5592221b185990b58b593ff34
hieubodoi <hieunvmph22325@fpt.edu.vn>
2023-04-11 22:30:47 +0700

    test

---
+++ DuAn1_Group5/src/ViewModels/hieutest.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ViewModels;
+
+/**
+ *
+ * @author Nguyễn Hiếu
+ */
+public class hieutest {
+    private String hieutest;
+    private String duan1;
+            
+}
